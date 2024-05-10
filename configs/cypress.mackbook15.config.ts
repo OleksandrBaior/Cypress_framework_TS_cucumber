df0d3c6d76a73cdd949e9b15d7ce8a6d4e699915
OleksandrBaior <OleksandrBaior@users.noreply.github.com>
2024-05-10 14:39:25 +0000

    Automated deployment: Fri May 10 14:39:25 UTC 2024 97413bd53e617bc49c84dee41b41302f88acd691

--- configs/cypress.mackbook15.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'cypress';
-import baseConfig from '../cypress.config';
-
-export default defineConfig({
-    ...baseConfig,
-    viewportWidth: 1440,
-    viewportHeight: 900,
-});
